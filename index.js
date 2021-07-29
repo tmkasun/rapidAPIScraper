07f9d0fd581fb4b85ef7ad21952f83c101bfdaa9
tmkasun <tmkasun@gmail.com>
2021-07-29 12:49:38 +0530

    adding VERIFIED_ONLY APIs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { GetApiOpenAPISpecs, GetProducts, extractSlugs, GetCollectionBySlug, Get
 import { randomInt } from './libs/utils.js'
 import fs from 'fs'
 
-const OFFICIAL_ONLY = true;
+const VERIFIED_ONLY = true;
+const OFFICIAL_ONLY = false;
 const slugs = await extractSlugs();
 
 const numberOfSlugs = OFFICIAL_ONLY ? 1 : 50;
@@ -11,32 +12,70 @@ for (let i of Array(numberOfSlugs).keys()) {
     const randomIndex = randomInt(0, slugs.length);
     const slugCollection = await GetCollectionBySlug(slugs[randomIndex]);
     const { id: collectionID, title } = slugCollection;
-    const dir = OFFICIAL_ONLY ? './data/official' : `./data/${title}`
+    let dir = `./data/${title}`;
+
+    if (VERIFIED_ONLY) {
+        dir = './data/verified'
+    } else if (OFFICIAL_ONLY) {
+        dir = './data/official';
+    }
     console.debug(`Creating ${dir} directory`)
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
-    const productsList = OFFICIAL_ONLY ? await GetProducts(OFFICIAL_ONLY) : await GetProducts(OFFICIAL_ONLY, [collectionID]);
-    for (const apiProduct of productsList) {
-        const { name, id, version: { id: versionId, tags } } = apiProduct;
-        const { publicdns } = await GetApiVersion(versionId);
-        const [publicDNS1] = publicdns;
-        console.log(`Fectching ${name} API prodcut`)
-        const dir = OFFICIAL_ONLY ? `./data/official/${name}` : `./data/${title}/${name}`
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
-        fs.writeFile(`${dir}/${name}.json`, JSON.stringify(apiProduct, null, 4), (err) => {
-            if (err) throw err;
-            console.log(`The ${name}.json API Meta has been saved!`);
-        });
-        const productOASDefinition = await GetApiOpenAPISpecs(apiProduct.id);
-        productOASDefinition.servers = productOASDefinition.servers.map(
-            ({ url, ...rest }) => Object.assign({ url: `https://${publicDNS1.address}${url}` }, rest)
-        )
-        fs.writeFile(`${dir}/${name}-OAS.json`, JSON.stringify(productOASDefinition, null, 4), (err) => {
-            if (err) throw err;
-            console.log(`The ${name}.json API OAS definition has been saved!`);
-        });
+    let result;
+    let pagination = { page: 1, size: 24, offset: 0 };
+    if (VERIFIED_ONLY || OFFICIAL_ONLY) {
+        result = await GetProducts([], VERIFIED_ONLY, OFFICIAL_ONLY, pagination);
     }
+    else {
+        result = await GetProducts([collectionID]);
+    }
+    let { results: productsList, total } = result;
+    let currentTotal = 0;
+    let hasNextPage = currentTotal < total;
+    while (hasNextPage) {
+        let pageCount = 0;
+        for (const apiProduct of productsList) {
+            pageCount += 1;
+            currentTotal += 1;
+            const { name, id, version: { id: versionId, tags } } = apiProduct;
+            const { publicdns } = await GetApiVersion(versionId);
+            const [publicDNS1] = publicdns;
+            console.log(`Fectching ${name} API prodcut`)
+            let dir = `./data/${title}/${name}`;
+            if (OFFICIAL_ONLY) {
+                dir = `./data/official/${name}`;
+            } else if (VERIFIED_ONLY) {
+                dir = `./data/verified/${name}`;
+            }
+
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            }
+            fs.writeFile(`${dir}/${name}.json`, JSON.stringify(apiProduct, null, 4), (err) => {
+                if (err) throw err;
+                console.log(`The ${name}.json API Meta has been saved!`);
+            });
+            const productOASDefinition = await GetApiOpenAPISpecs(apiProduct.id);
+            productOASDefinition.servers = productOASDefinition.servers.map(
+                ({ url, ...rest }) => Object.assign({ url: `https://${publicDNS1.address}${url}` }, rest)
+            )
+            fs.writeFile(`${dir}/${name}-OAS.json`, JSON.stringify(productOASDefinition, null, 4), (err) => {
+                if (err) throw err;
+                console.log(`The ${name}.json API OAS definition has been saved!`);
+            });
+            console.warn(`Saved ${pageCount} APIs out of ${productsList.length} in ${pagination.page} page with total ${currentTotal}/${total} `);
+        }
+        if (VERIFIED_ONLY) {
+            pagination.page = pagination.page + 1;
+            const response = await GetProducts([], VERIFIED_ONLY, OFFICIAL_ONLY, pagination);
+            productsList = response.results;
+            total = response.total;
+            hasNextPage = currentTotal < total;
+        } else {
+            hasNextPage = false; // Do not iterate
+        }
+    }
+
 }
--- libs/GetProducts.js
+++ libs/GetProducts.js
@@ -1,23 +1,34 @@
 import { GQL_ENDPOINT, credentials } from "./config.js";
 import fetch from 'node-fetch';
 
-export const GetProducts = async (isOfficial = false, collectionIds = []) => {
+export const GetProducts = async (collectionIds = [], isVerified = false, isOfficial = false, pagination = { page: 1, size: 24, offset: 0 }) => {
   const { csrfToken, _csrf, jwt_auth } = credentials;
+  const tags = [];
+  if (isVerified) {
+    tags.push([
+      "tagdefinition_ad8279d5-df5e-40b5-86f6-0c467a403932",
+      [
+        "✓"
+      ]
+    ]);
+  }
+  if (isOfficial) {
+    tags.push([
+      "tagdefinition_0364d241-f423-4ff4-9e3e-e51de3cdc835",
+      [
+        "Official"
+      ]
+    ]);
+  }
+  const { page, size, offset } = pagination;
   const variables = {
     "searchArgs": {
-      "page": 1,
-      "size": 24,
-      "offset": 0,
+      page,
+      size,
+      offset,
       "sortBy": "installsAllTime",
       "term": "",
-      "tags": isOfficial ? [
-        [
-          "tagdefinition_0364d241-f423-4ff4-9e3e-e51de3cdc835",
-          [
-            "Official"
-          ]
-        ]
-      ] : [],
+      tags,
       collectionIds,
       "categoryNames": [],
       "privateApisJwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnZpdGVkQVBJc0lEcyI6W10sImlhdCI6MTYyNTU3MTcwNX0.60ngcZqM9u-kJJTU8fi4HsHrQZ4lXjCt11VTV-dosZc"
@@ -94,7 +105,7 @@ export const GetProducts = async (isOfficial = false, collectionIds = []) => {
         variables,
       }),
     }).then(r => r.json());
-    return data.data.products.results;
+    return data.data.products;
   } catch (error) {
     console.error(error);
   }
